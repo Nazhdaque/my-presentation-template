cc86829e2ef09667b1de55fddba85b12432754c4
nazhdaque <nazhdaque@gmail.com>
2023-10-03 19:39:23 +0300

    test-5

--- js/importHTML.js
+++ js/importHTML.js
@@ -1,10 +1,10 @@
 // import headContent from "../html/parts/headContent.html?raw";
 // import headerContent from "../html/parts/headerContent.html?raw";
-// import sidebarContent from "../html/parts/sidebarContent.html?raw";
-// import slide_1 from "../html/parts/slide-1.html?raw";
-// import slide_2 from "../html/parts/slide-2.html?raw";
-// import slide_3 from "../html/parts/slide-3.html?raw";
-// import slide_4 from "../html/parts/slide-4.html?raw";
+import sidebarContent from "../html/parts/sidebarContent.html?raw";
+import slide_1 from "../html/parts/slide-1.html?raw";
+import slide_2 from "../html/parts/slide-2.html?raw";
+import slide_3 from "../html/parts/slide-3.html?raw";
+import slide_4 from "../html/parts/slide-4.html?raw";
 // import slide_5 from "../html/parts/slide-5.html?raw";
 // import slide_6 from "../html/parts/slide-6.html?raw";
 // import slide_7 from "../html/parts/slide-7.html?raw";
@@ -20,14 +20,14 @@
 // const header = document.querySelector("header");
 // header.insertAdjacentHTML("beforeend", headerContent);
 
-// const sidebar = document.querySelector(".sidebar");
-// sidebar.insertAdjacentHTML("beforeend", sidebarContent);
+const sidebar = document.querySelector(".sidebar");
+sidebar.insertAdjacentHTML("beforeend", sidebarContent);
 
-// const slidesContainer = document.querySelector(".swiper-wrapper");
-// slidesContainer.insertAdjacentHTML("beforeend", slide_1);
-// slidesContainer.insertAdjacentHTML("beforeend", slide_2);
-// slidesContainer.insertAdjacentHTML("beforeend", slide_3);
-// slidesContainer.insertAdjacentHTML("beforeend", slide_4);
+const slidesContainer = document.querySelector(".swiper-wrapper");
+slidesContainer.insertAdjacentHTML("beforeend", slide_1);
+slidesContainer.insertAdjacentHTML("beforeend", slide_2);
+slidesContainer.insertAdjacentHTML("beforeend", slide_3);
+slidesContainer.insertAdjacentHTML("beforeend", slide_4);
 // slidesContainer.insertAdjacentHTML("beforeend", slide_5);
 // slidesContainer.insertAdjacentHTML("beforeend", slide_6);
 // slidesContainer.insertAdjacentHTML("beforeend", slide_7);
