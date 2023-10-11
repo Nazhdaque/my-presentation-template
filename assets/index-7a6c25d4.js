3c39e82518956fa196c40eae2ebb34ecb93f1360
nazhdaque <nazhdaque@gmail.com>
2023-10-11 18:03:23 +0300

    Updates

--- assets/index-7a6c25d4.js
+++ assets/index-7a6c25d4.js
@@ -541,7 +541,7 @@ var ea=Object.defineProperty;var ia=(i,t,e)=>t in i?ea(i,t,{enumerable:!0,config
 						<div class="corners-1"></div>
 						<div class="corners-2"></div>
 						<div class="glitch"
-								 style="background-image: url(/images/demo-img-360p.webp)">
+								 style="background-image: url(images/demo-img-360p.webp)">
 							<div class="channel r"></div>
 							<div class="channel g"></div>
 							<div class="channel b"></div>
