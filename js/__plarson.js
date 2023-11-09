6a9a92d4ac7d8baca466012f0c9aa561d07b4da4
nazhdaque <nazhdaque@gmail.com>
2023-11-09 14:11:35 +0300

    plarson js

--- js/__plarson.js
+++ js/__plarson.js
@@ -1,13 +1,20 @@
-// --- PLARSON
-const logMessage = () =>
+const beforeMsg = "документ прочитан";
+const afterMsg = "документ прочитан, ресурсы загружены";
+
+const logMessage = msg =>
 	console.log(
-		"%cPLARSON test message: страница полностью загружена",
-		"background: #222; color: magenta;"
+		`%cPLARSON test message: ${msg}`,
+		`background: #222; color: magenta;`
 	);
 
 const completeTest = () => {
-	document.onreadystatechange = () => {
-		document.readyState === "complete" && logMessage();
+	document.onreadystatechange = e => {
+		switch (e.target.readyState) {
+			case "interactive":
+				return logMessage(beforeMsg);
+			case "complete":
+				return logMessage(afterMsg);
+		}
 	};
 };
 
