1b5a8eae4e61d3c211768b16bb663b90369d9549
nazhdaque <nazhdaque@gmail.com>
2023-12-25 19:29:18 +0300

    111

--- js/MobileDesktopStatesManager.js
+++ js/MobileDesktopStatesManager.js
@@ -6,18 +6,17 @@ export class MobileDesktopStatesManager {
 
 	setState = isMobileSize =>
 		isMobileSize ? this.onMobile() : this.onDesktop();
-	chooseState = isMobileSize => this.setState(isMobileSize);
-	checkState = breakpoint => {
+
+	checkState = breakpoint =>
 		window.innerWidth <= breakpoint
 			? this.setState(true)
 			: this.setState(false);
-	};
-	watchState = breakpoint => {
-		const mediaQueryList = window.matchMedia(`(max-width: ${breakpoint}px)`);
-		mediaQueryList.addEventListener("change", event =>
-			this.chooseState(event.matches)
-		);
-	};
+
+	watchState = breakpoint =>
+		window
+			.matchMedia(`(max-width: ${breakpoint}px)`)
+			.addEventListener("change", e => this.setState(e.matches));
+
 	toggleStateOn = breakpoint => {
 		this.checkState(breakpoint);
 		this.watchState(breakpoint);
